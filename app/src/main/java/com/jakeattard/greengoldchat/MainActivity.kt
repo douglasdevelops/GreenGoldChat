2e3da65df9a0619a3ca1187c1a7a6de976e96adf
Jake Attard <jakeattard18@gmail.com>
2018-10-13 19:44:53 +1000

    Features added

--- app/src/main/java/com/jakeattard/greengoldchat/MainActivity.kt
+++ app/src/main/java/com/jakeattard/greengoldchat/MainActivity.kt
@@ -3,6 +3,8 @@ package com.jakeattard.greengoldchat
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,8 +19,19 @@ class MainActivity : AppCompatActivity() {
             val email = emailRegisterScrn.text.toString()
             val password = passwordRegisterScrn.text.toString()
 
+            if(email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             // Firebase Authentication
-            FirebaseAuth.getInstance()
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener{
+                        if (!it.isSuccessful) return@addOnCompleteListener
+
+                        // else if successful
+                        Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+                    }
 
         }
         loginTextViewRegister.setOnClickListener {
