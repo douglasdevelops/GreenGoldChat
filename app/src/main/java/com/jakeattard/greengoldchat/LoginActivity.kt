1aab4ea70907c232c602911b19b859ef686f4f28
Jake Attard <jakeattard18@gmail.com>
2018-10-13 16:49:02 +1000

    Login Screen & Bug fixes

--- app/src/main/java/com/jakeattard/greengoldchat/LoginActivity.kt
+++ app/src/main/java/com/jakeattard/greengoldchat/LoginActivity.kt
@@ -2,11 +2,21 @@ package com.jakeattard.greengoldchat
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginBtnScrn.setOnClickListener {
+            val email = emailLoginScrn.text.toString()
+            val password = passwordLoginScrn.text.toString()
+        }
+
+        registerTextViewScrn.setOnClickListener {
+            finish()
+        }
     }
 }
